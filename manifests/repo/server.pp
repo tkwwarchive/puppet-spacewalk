f035f3d93f732609c13bcf6541744502f56d1cc6
Borja Juncosa <bjuncosa@bodas.net>
2019-04-12 13:22:10 +0200

    Make repo server more flexible

--- manifests/repo/server.pp
+++ manifests/repo/server.pp
@@ -1,12 +1,12 @@
 class spacewalk::repo::server (
   $spacewalk_repo_enabled   = '1',
   $spacewalk_repo_gpgcheck  = '1',
-  $spacewalk_repo_release   = 'latest',
-  $spacewalk_repo_gpgkey    = 'https://copr-be.cloud.fedoraproject.org/results/@spacewalkproject/spacewalk-2.8/pubkey.gpg',
+  $spacewalk_repo_release   = '2.8',
+  $spacewalk_repo_gpgkey    = "https://copr-be.cloud.fedoraproject.org/results/@spacewalkproject/spacewalk-${spacewalk_repo_release}/pubkey.gpg",
 
-  $copr_repo_enabled    = '1',
-  $copr_repo_gpgcheck   = '1',
-  $copr_repo_gpgkey     = 'https://copr-be.cloud.fedoraproject.org/results/@spacewalkproject/java-packages/pubkey.gpg',
+  $copr_repo_enabled        = '1',
+  $copr_repo_gpgcheck       = '1',
+  $copr_repo_gpgkey         = 'https://copr-be.cloud.fedoraproject.org/results/@spacewalkproject/java-packages/pubkey.gpg',
 ){
 
   case $::osfamily {
@@ -17,7 +17,7 @@ class spacewalk::repo::server (
         descr    => "Spacewalk ${spacewalk_repo_release} Repository",
         gpgcheck => $spacewalk_repo_gpgcheck,
         gpgkey   => $spacewalk_repo_gpgkey,
-        baseurl  => "https://copr-be.cloud.fedoraproject.org/results/@spacewalkproject/spacewalk-2.8/fedora-${::operatingsystemmajrelease}-\$basearch/",
+        baseurl  => "https://copr-be.cloud.fedoraproject.org/results/@spacewalkproject/spacewalk-${spacewalk_repo_release}/epel-${::operatingsystemmajrelease}-\$basearch/",
     }
 
       yumrepo {'copr-java':
@@ -25,7 +25,7 @@ class spacewalk::repo::server (
         descr         => 'Copr repo for java packages owned by @spacewalkproject',
         gpgcheck      => $copr_repo_gpgcheck,
         gpgkey        => $copr_repo_gpgkey,
-        baseurl       => "https://copr-be.cloud.fedoraproject.org/results/%40spacewalkproject/java-packages/epel-7-x86_64/",
+        baseurl       => "https://copr-be.cloud.fedoraproject.org/results/@spacewalkproject/java-packages/epel-${::operatingsystemmajrelease}-\$basearch/",
         repo_gpgcheck => '0',
       }
     }
