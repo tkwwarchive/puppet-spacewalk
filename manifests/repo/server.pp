ac68c1a9b43774ec34e62608140f492429579607
Borja Juncosa <bjuncosa@bodas.net>
2019-04-15 09:16:19 +0200

    Fix Fedorda compatibility

--- manifests/repo/server.pp
+++ manifests/repo/server.pp
@@ -7,17 +7,22 @@ class spacewalk::repo::server (
   $copr_repo_enabled        = '1',
   $copr_repo_gpgcheck       = '1',
   $copr_repo_gpgkey         = 'https://copr-be.cloud.fedoraproject.org/results/@spacewalkproject/java-packages/pubkey.gpg',
+  $epel_release             = '7',
 ){
 
   case $::osfamily {
     'RedHat': {
-
+      $repo_name = $::os['name'] ? {
+        'Fedora' => 'fedora',
+        'CentOS' => 'epel',
+        default  => 'epel'
+      }
       yumrepo {'spacewalk-project':
         enabled  => $spacewalk_repo_enabled,
         descr    => "Spacewalk ${spacewalk_repo_release} Repository",
         gpgcheck => $spacewalk_repo_gpgcheck,
         gpgkey   => $spacewalk_repo_gpgkey,
-        baseurl  => "https://copr-be.cloud.fedoraproject.org/results/@spacewalkproject/spacewalk-${spacewalk_repo_release}/epel-${::operatingsystemmajrelease}-\$basearch/",
+        baseurl  => "https://copr-be.cloud.fedoraproject.org/results/@spacewalkproject/spacewalk-${spacewalk_repo_release}/${repo_name}-${::operatingsystemmajrelease}-\$basearch/",
     }
 
       yumrepo {'copr-java':
@@ -25,7 +30,7 @@ class spacewalk::repo::server (
         descr         => 'Copr repo for java packages owned by @spacewalkproject',
         gpgcheck      => $copr_repo_gpgcheck,
         gpgkey        => $copr_repo_gpgkey,
-        baseurl       => "https://copr-be.cloud.fedoraproject.org/results/@spacewalkproject/java-packages/epel-${::operatingsystemmajrelease}-\$basearch/",
+        baseurl       => "https://copr-be.cloud.fedoraproject.org/results/@spacewalkproject/java-packages/epel-${epel_release}-\$basearch/",
         repo_gpgcheck => '0',
       }
     }
